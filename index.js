6ac5178b0753b4f77441dbef896108cdf3ed06db
JorgeR-Hi <jrjorgemessi@gmail.com>
2023-05-22 20:08:01 -0500

    seed script

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const PORT = process.env.PORT;
 const express = require("express");
 const server = express();
-require("dotenv").config();
 
 //console.log(process.env.JWT_SECRET)
 
